3633081bcc47a3582cc2b688b6ff7e4041bbb8cd
Borys Ushakov <inlvert@gmail.com>
2024-03-29 12:25:41 +0200

    added CRUD for power

--- controllers/power.controller.js
+++ controllers/power.controller.js
@@ -1,3 +1,4 @@
+const createHttpError = require("http-errors");
 const { Power } = require("../models");
 
 module.exports.createPower = async (req, res, next) => {
@@ -24,3 +25,72 @@ module.exports.getAllSuperheroPower = async (req, res, next) => {
   }
 }
 
+module.exports.getPower = async (req, res, next) => {
+  try {
+    const { params: { powerId }, superhero } = req;
+
+    const power = await Power.findOne({
+      where: {
+        id: powerId,
+        superheroId: superhero.id
+      }
+    })
+
+    if(!power) {
+      return next(createHttpError(404, 'power not found'))
+    }
+
+    res.send({data: power});
+
+  } catch (error) {
+    next(error);
+  }
+};
+
+
+module.exports.updatePower = async (req, res, next) => {
+  try {
+    const {superhero, params: {powerId}, body} = req;
+
+    const [updateCount, [power]] = await Power.update(body, {
+      where: {
+        id: powerId,
+        superheroId: superhero.id
+      },
+      returning: true
+    });
+    
+    if(updateCount !== 1) {
+      return next(createHttpError(404, 'power not found'))
+    }
+
+    res.send({data: power});
+
+  } catch (error) {
+    next(error)
+  }
+};
+
+module.exports.deletePower = async (req, res, next) => {
+  try {
+    const {superhero, params: {powerId}} =req;
+
+    const power = await Power.findOne({
+      where: {
+        id: powerId,
+        superheroId: superhero.id
+      }
+    });
+
+    if(!power) {
+      return next(createHttpError(404, 'power not found'))
+    }
+
+    await power.destroy();
+
+    res.send({data: power})
+
+  } catch (error) {
+    next(error);
+  }
+}
--- routers/powerRouter.js
+++ routers/powerRouter.js
@@ -3,5 +3,9 @@ const powerController = require('../controllers/power.controller');
 
 powerRouter.post('/', powerController.createPower);
 powerRouter.get('/', powerController.getAllSuperheroPower);
+powerRouter.get('/:powerId', powerController.getPower);
+powerRouter.put('/:powerId', powerController.updatePower);
+powerRouter.delete('/:powerId', powerController.deletePower);
+
 
 module.exports = powerRouter;
